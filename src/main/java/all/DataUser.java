dcf6981745bcbae5643d8d743a2620eec4b594b0
IvanovaAnn <a.060999@yandex.ru>
2019-10-26 23:47:42 +0500

    Исправлен map, теперь по chatIp ищется экземпляр класс DataUser, создан класс DataUser, появлись кнопки выбора языка, убран вспомогательный метод с кавычками

---
+++ src/main/java/all/DataUser.java
@@ -0,0 +1,6 @@
+package all;
+
+public class DataUser {
+    public int index;
+    public Question[] currentData;
+}
--- src/main/java/telegram/TelegramBot.java
+++ src/main/java/telegram/TelegramBot.java
@@ -1,6 +1,8 @@
 package telegram;
 
+import all.DataUser;
 import all.Question;
+import all.User;
 import all.WorkWithJson;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -18,11 +20,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     private static final String USERNAME = "@language_programming_bot";
     private static final Question[] data_py = WorkWithJson.readJson("question.json");
     private static final Question[] data_sharp = WorkWithJson.readJson("question_sharp.json");
-    //создал второй джсон для вопросов по другой теме, переменная ниже будет получать значение либо парса вопросов по питону,либо по шарпу, в зависимости от того,какую тему мы выбрали,
-    //сейчас я не доделал эту фичу и по умолчанию стоит джсон с питоном
-    //я хочу прикрутить это к команде старт,чтобы на нее он давал выбор темы а потом уже в основном методе крутились вопросы
-    private static Question[] currentData = data_py;
-    public static Map<Long, Integer> map = new HashMap<Long, Integer>();
+    private static Map<Long, DataUser> map = new HashMap<>();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -32,14 +30,14 @@ public class TelegramBot extends TelegramLongPollingBot {
                 processCommand(update);
             }
         }
-        try {
-            chatId = update.getCallbackQuery().getMessage().getChatId();
-            execute(new SendMessage().setText("").setChatId(chatId));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
         if (update.hasCallbackQuery()) {
-            processCallBack(update, chatId);
+            chatId = update.getCallbackQuery().getMessage().getChatId();
+            if (map.get(chatId).index == -1) {
+                answerOfCheckLanguage(update, chatId);
+            }
+            else {
+                processCallBack(update, chatId);
+            }
         }
         try {
             execute(sendInlineKeyboardMessage(chatId, getRandomQuestionWithAnswers(chatId)));
@@ -56,7 +54,6 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     public void processCommand(Update update) {
-        //я не догоняю как тут надо сделать с чат айди, если неверно, то фикси
         if (update.getMessage().getText().equals("/help")) {
             try {
                 SendMessage message = getCommands();
@@ -70,8 +67,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.getMessage().getText().equals("/start")) {
             Long chatId = update.getMessage().getChatId();
             try {
-                //хочу чтобы тут выдавались кнопки для выбора темы,а дальше все работало,но не могу сделать,получается либо кнопки сделать а потом ничего не работает,либо сразу идут вопросы,как сейчас
-                execute(sendInlineKeyboardMessage(chatId, getRandomQuestionWithAnswers(chatId)));
+                execute(checkLanguage(chatId, "По какому языку вы хотите тест?"));
+                map.remove(chatId);
+                DataUser newUser = new DataUser();
+                newUser.index = -1;
+                map.put(chatId, newUser);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
@@ -87,6 +87,28 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private static SendMessage checkLanguage(long chatId, String question) {
+        InlineKeyboardMarkup inlineKM = new InlineKeyboardMarkup();
+        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
+        InlineKeyboardButton button = new InlineKeyboardButton().setText("Python").setCallbackData("Python");
+        keyboardButtonsRow.add(button);
+        InlineKeyboardButton buttonTwo = new InlineKeyboardButton().setText("C#").setCallbackData("C#");
+        keyboardButtonsRow.add(buttonTwo);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow);
+        inlineKM.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText(question).setReplyMarkup(inlineKM);
+    }
+
+    private void answerOfCheckLanguage(Update update, Long chatId) {
+        SendMessage userAnswer = new SendMessage().setText(update.getCallbackQuery().getData()).setChatId(update.getCallbackQuery().getMessage().getChatId());
+        if (userAnswer.getText().equals("Python")) {
+            map.get(chatId).currentData = data_py;
+        } else if (userAnswer.getText().equals("C#")){
+            map.get(chatId).currentData  = data_sharp;
+        }
+    }
+
     @Override
     public String getBotUsername() {
         return USERNAME;
@@ -96,20 +118,6 @@ public class TelegramBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return TOKEN;
     }
-
-//    private static SendMessage chooseTheme(long chatId) {
-//        //копирует метод ниже, но я не придумал,как сделать общий, здесь создаются кнопки для выбора темы
-//        InlineKeyboardMarkup inlineKM = new InlineKeyboardMarkup();
-//        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
-//        InlineKeyboardButton button1 = new InlineKeyboardButton().setText("Python").setCallbackData("Python");
-//        keyboardButtonsRow.add(button1);
-//        InlineKeyboardButton button2 = new InlineKeyboardButton().setText("C#").setCallbackData("C#");
-//        keyboardButtonsRow.add(button2);
-//        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
-//        rowList.add(keyboardButtonsRow);
-//        inlineKM.setKeyboard(rowList);
-//        return new SendMessage().setChatId(chatId).setText("Выберите: ").setReplyMarkup(inlineKM);
-//    }
 
     private static SendMessage sendInlineKeyboardMessage(long chatId, String question) {
         InlineKeyboardMarkup inlineKM = new InlineKeyboardMarkup();
@@ -126,42 +134,28 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private static String getRandomQuestionWithAnswers(Long chatId) {
         Random random = new Random();
+        DataUser newUser = new DataUser();
+        newUser.index = random.nextInt(map.get(chatId).currentData.length);
+        newUser.currentData = map.get(chatId).currentData;
         map.remove(chatId);
-        map.put(chatId, random.nextInt(currentData.length));
-        return currentData[map.get(chatId)].question + "\n" + currentData[map.get(chatId)].formatAnswers();
+        map.put(chatId, newUser);
+        return map.get(chatId).currentData[map.get(chatId).index].question + "\n"
+                + map.get(chatId).currentData[map.get(chatId).index].formatAnswers();
     }
 
-//    private void processChooseTheme(Update update, Long chatId) {
-//        SendMessage userAnswer = new SendMessage().setText(update.getCallbackQuery().getData()).setChatId(update.getCallbackQuery().getMessage().getChatId());
-//        if (userAnswer.getText().equals("Python")) {
-//            currentData = data_py;
-//            try {
-//                execute(sendInlineKeyboardMessage(chatId, getRandomQuestionWithAnswers(chatId)));
-//            } catch (TelegramApiException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        if (userAnswer.getText().equals("C#")) {
-//            currentData = data_sharp;
-//            try {
-//                execute(sendInlineKeyboardMessage(chatId, getRandomQuestionWithAnswers(chatId)));
-//            } catch (TelegramApiException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
     private void processCallBack(Update update, Long chatId) {
-        SendMessage userAnswer = new SendMessage().setText(update.getCallbackQuery().getData()).setChatId(update.getCallbackQuery().getMessage().getChatId());
-        if (userAnswer.getText().equals(currentData[map.get(chatId)].correct)) {
+        SendMessage userAnswer =
+                new SendMessage().setText(update.getCallbackQuery().getData()).setChatId(update.getCallbackQuery().getMessage().getChatId());
+        if (userAnswer.getText().equals(map.get(chatId).currentData[map.get(chatId).index].correct)) {
             try {
                 execute(new SendMessage().setText("Да, ваш ответ верный!").setChatId(update.getCallbackQuery().getMessage().getChatId()));
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-        } else if (!userAnswer.getText().equals(currentData[map.get(chatId)].correct)){
+        } else if (!userAnswer.getText().equals(map.get(chatId).currentData[map.get(chatId).index].correct)){
             try {
-                String str = "Нет, ваш ответ неверный, дополнительная информация на эту тему:" + " " + currentData[map.get(chatId)].link;
+                String str = "Нет, ваш ответ неверный, дополнительная информация на эту тему:" + " "
+                        + map.get(chatId).currentData[map.get(chatId).index].link;
                 execute(new SendMessage().setText(str).setChatId(update.getCallbackQuery().getMessage().getChatId()));
             } catch (TelegramApiException e) {
                 e.printStackTrace();
